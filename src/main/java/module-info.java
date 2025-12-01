192595f089f9d790d0d09837f3d47416ed7e40a9
mohammedrattia <mohammedr25102005@gmail.com>
2025-12-01 11:25:11 +0200

    making test package for me

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,13 @@ module com.hypermarket {
     requires javafx.fxml;
 
     opens com.hypermarket to javafx.fxml;
+
     exports com.hypermarket;
+
+    // making a testing package
+    // Open to EVERYONE (Reflection/FXML)
+    opens com.hypermarket.ragab;
+
+    // Export to EVERYONE (Public access)
+    exports com.hypermarket.ragab;
 }
