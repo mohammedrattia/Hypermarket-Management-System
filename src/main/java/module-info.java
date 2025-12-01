415ddb088c3e930a331601b33eeec1aaedb4432a
mohammedrattia <mohammedr25102005@gmail.com>
2025-12-01 14:39:02 +0200

    editing module-info.java to remove an warning in App.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.hypermarket {
-    requires javafx.controls;
-    requires javafx.fxml;
+    requires transitive javafx.fxml;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
 
     opens com.hypermarket to javafx.fxml;
 
@@ -8,7 +9,7 @@ module com.hypermarket {
 
     // making a testing package
     // Open to EVERYONE (Reflection/FXML)
-    opens com.hypermarket.ragab;
+    opens com.hypermarket.ragab to javafx.fxml;
 
     // Export to EVERYONE (Public access)
     exports com.hypermarket.ragab;
