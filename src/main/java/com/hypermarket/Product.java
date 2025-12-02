88f01900d821d6b839236f7995bcb384f244f3a5
Ahmed Mohamed <ahmed2naruto@gmail.com>
2025-12-02 17:30:39 +0200

    Product Info Add Method

--- src/main/java/com/hypermarket/Product.java
+++ src/main/java/com/hypermarket/Product.java
@@ -1,5 +1,29 @@
 package com.hypermarket;
 
 public class Product {
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
+    private String size;
+    private String duration;
+    private String imagePath;
 
+    Product(String name, String category, double price, int quantity, String size, String duration, String imagePath) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+        this.size = size;
+        this.duration = duration;
+        this.imagePath = imagePath;
+    }
+
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+    public String getSize() { return size; }
+    public String getDuration() { return duration; }
+    public String getImagePath() { return imagePath; }
 }
