d0f7d119b8246f5911aa8fb4e0697a0ccf9588b7
hanamohvmed <haniaimos6@gmail.com>
2025-12-02 17:14:35 +0200

    creating controllers for employee cards

--- src/main/java/com/hypermarket/Employee.java
+++ src/main/java/com/hypermarket/Employee.java
@@ -1,5 +1,28 @@
 package com.hypermarket;
 
 public class Employee {
+    private String name;
+    private String title;
+    private String salary;
+    private String phone;
 
+    public Employee(String name, String title, String salary, String phone) {
+        this.name = name;
+        this.title = title;
+        this.salary = salary;
+        this.phone = phone;
+    }
+
+    public String getName() { 
+        return name;
+    }
+    public String getTitle() { 
+        return title;
+    }
+    public String getSalary() { 
+        return salary;
+    }
+    public String getPhone() { 
+        return phone;
+    }
 }
