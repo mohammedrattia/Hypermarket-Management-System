351361d1b37a16591ba22dc2181ebf6b9710eba7
Ahmed Mohamed <ahmed2naruto@gmail.com>
2025-12-02 10:50:33 +0200

    PieChart Added

--- src/main/java/com/hypermarket/App.java
+++ src/main/java/com/hypermarket/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("ListElement"));
+        scene = new Scene(loadFXML("PieChart"));
         stage.setScene(scene);
         stage.show();
     }
