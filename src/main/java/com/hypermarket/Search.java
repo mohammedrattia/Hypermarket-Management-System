a94dbd1cd5e5ba6ac3a651cb8058a4517f04bcae
mohammedrattia <mohammedr25102005@gmail.com>
2025-12-02 18:21:09 +0200

    edit a todo

--- src/main/java/com/hypermarket/Search.java
+++ src/main/java/com/hypermarket/Search.java
@@ -3,8 +3,8 @@ package com.hypermarket;
 import javafx.collections.transformation.FilteredList;
 
 public class Search {
-    // TODO: function that takes filteredData and filter it based on newValue of a
-    // specific Property
+    // TODO: function that takes filteredData and filter it
+    // based on newValue of a specific Property
     public static <T> FilteredList<T> addFilter(FilteredList<T> filteredData, String newValue,
             String filterOnProperty) {
         System.out.println("hellooooooo the function is here ");
