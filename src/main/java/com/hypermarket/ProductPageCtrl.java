4726699cfa66319a1e04187b340049109b60fbf0
Ahmed Mohamed <ahmed2naruto@gmail.com>
2025-12-02 18:35:22 +0200

    offer and description added

--- src/main/java/com/hypermarket/ProductPageCtrl.java
+++ src/main/java/com/hypermarket/ProductPageCtrl.java
@@ -11,18 +11,21 @@ public class ProductPageCtrl {
     @FXML private Label productNameLabel;
     @FXML private Label categoryLabel;
     @FXML private Label priceLabel;
+    @FXML private Label offerLabel;
     @FXML private Label quantityLabel;
     @FXML private Label sizeLabel;
     @FXML private Label durationLabel;
+    @FXML private Label productID;
     @FXML private ImageView productImage;
 
     public void setProduct(Product product) {
         productNameLabel.setText(product.getName());
         categoryLabel.setText("Category : " + product.getCategory());
         priceLabel.setText("Price : $" + product.getPrice());
+        priceLabel.setText("Offer : $" + product.getOffer());
         quantityLabel.setText("Quantity : " + product.getQuantity());
         sizeLabel.setText("Size : " + product.getSize());
         durationLabel.setText("Duration : " + product.getDuration());
+        productID.setText("Product ID : " + product.getProductID());
     }
-
 }
