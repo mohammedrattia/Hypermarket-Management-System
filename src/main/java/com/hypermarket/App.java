80cc82834e10a43112ed13800985845cdfe92377
hanamohvmed <haniaimos6@gmail.com>
2025-12-04 22:12:15 +0200

    removing files

--- src/main/java/com/hypermarket/App.java
+++ src/main/java/com/hypermarket/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("EmployeeCardsFull"));
+        scene = new Scene(loadFXML("Piechart"));
         stage.setScene(scene);
         stage.show();
     }
