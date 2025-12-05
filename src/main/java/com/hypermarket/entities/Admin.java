0821f783190d82ec48d053eb40ab3ef63cd78802
hanamohvmed <haniaimos6@gmail.com>
2025-12-05 12:31:53 +0200

    intial Admin class

--- src/main/java/com/hypermarket/entities/Admin.java
+++ src/main/java/com/hypermarket/entities/Admin.java
@@ -1,5 +1,18 @@
 package com.hypermarket.entities;
 
-public class Admin {
+public class Admin extends User{
+    public Admin(Role role, int id, String fname, String lname,String phone, String email, String password, double salary){
+        super(role,id,fname,lname,phone,email,password,salary);
+    }
 
+    public void addUser(Role role, int id, String fname, String lname,String phone, String email, String password, double salary){
+
+    }
+
+    public void updateUser(int id, User user){
+    }
+
+    public void deleteUser(int id){
+
+    }
 }
