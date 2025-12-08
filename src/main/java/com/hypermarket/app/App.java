c7a9f7ef27c4bc5ef62d5eb48a0cc6a0d6a1f3ca
hanamohvmed <haniaimos6@gmail.com>
2025-12-08 20:39:36 +0200

    Add AdminView Controller, Employee Grid and Dashboard Home. + Adding SideBar navigation logic and title change

--- src/main/java/com/hypermarket/app/App.java
+++ src/main/java/com/hypermarket/app/App.java
@@ -1,91 +1,39 @@
 package com.hypermarket.app;
 
-// import com.hypermarket.modules.components.*;
-// import com.hypermarket.entities.*;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import com.hypermarket.data.*;
-import com.hypermarket.entities.*;
-import com.hypermarket.modules.login.LoginController;
-import com.hypermarket.service.Session;
-
 /**
+ * 
  * JavaFX App
  */
 public class App extends Application {
 
-        private static Scene scene;
+    private static Scene scene;
 
-        @Override
-        public void start(Stage stage) throws IOException {
-                DataStore.getDataStore().loadAllData();
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("/com/hypermarket/view/admin/AdminView"));
+        stage.setScene(scene);
+        stage.show();
+    }
 
-                FXMLLoader loginLoader = loadLoginScene(stage);
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
 
-                LoginController controller = loginLoader.getController();
-                controller.setOnLoginSuccess(() -> {
-                        User currentUser = Session.getInstance().getUser();
-                        try {
-                                switch (currentUser.getRole()) {
-                                        case Role.ADMIN:
-                                                loadAdminScene(stage);
-                                                break;
-                                        case Role.SALES:
-                                                loadSalesScene(stage);
-                                                break;
-                                        case Role.INVENTORY:
-                                                loadInventoryScene(stage);
-                                                break;
-                                        case Role.MARKETING:
-                                                loadMarketingScene(stage);
-                                                break;
-                                }
-                        } catch (IOException e) {
-                                e.printStackTrace();
-                        }
-                });
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
 
-        }
+        return fxmlLoader.load();
+    }
 
-        private static FXMLLoader loadLoginScene(Stage stage) throws IOException {
-                FXMLLoader fxmlLoader = new FXMLLoader(
-                                App.class.getResource("/com/hypermarket/view/login/Login.fxml"));
-                scene = new Scene(fxmlLoader.load());
-
-                stage.setScene(scene);
-                stage.show();
-                return fxmlLoader;
-        }
-
-        private static void loadAdminScene(Stage stage) throws IOException {
-                FXMLLoader fxmlLoader = new FXMLLoader(
-                                App.class.getResource("/com/hypermarket/view/admin/AdminDashboard.fxml"));
-
-                scene = new Scene(fxmlLoader.load());
-                stage.setScene(scene);
-                stage.show();
-        }
-
-        private static void loadSalesScene(Stage stage) throws IOException {
-
-        }
-
-        private static void loadInventoryScene(Stage stage) throws IOException {
-
-        }
-
-        private static void loadMarketingScene(Stage stage) throws IOException {
-
-        }
-
-        public static void main(String[] args) {
-                launch();
-        }
-
+    public static void main(String[] args) {
+        launch();
+    }
 }
