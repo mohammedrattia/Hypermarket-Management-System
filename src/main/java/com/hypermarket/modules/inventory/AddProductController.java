1630a5b7341a3660c64eb823295a2191e2c9d3ea
Ahmed Mohamed <ahmed2naruto@gmail.com>
2025-12-13 23:56:51 +0200

    AddProd#3

--- src/main/java/com/hypermarket/modules/inventory/AddProductController.java
+++ src/main/java/com/hypermarket/modules/inventory/AddProductController.java
@@ -3,9 +3,13 @@ package com.hypermarket.modules.inventory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.scene.image.Image;
 
 public class AddProductController {
@@ -25,21 +29,31 @@ public class AddProductController {
     @FXML
     private TextField categoryField;
 
-    // @FXML
-    // private ImageView productImageView;
-
-    // private String imagePath = null; 
+    @FXML
+    private ImageView productImageView;
 
     @FXML
     private Button clearBtn;
 
+    @FXML
+    private Button AddImageButton;
+
+    private File selectedImageFile;
+    private int placeholderID;
+    private String imageName;
 
     @FXML
     public void initialize() {
+        imageName = "user_" + String.format("%03d", placeholderID) + ".png";
         clearBtn.setOnAction(event -> {
             clearAllFields();
         });
+
+        AddImageButton.setOnAction(event -> {
+            handleImageSelection();
+        });
     }
+
 
     @FXML
     void clearAllFields() {
@@ -48,10 +62,28 @@ public class AddProductController {
         priceField.clear();
         categoryField.clear();
         descriptionField.clear();
-        // productImageView.setImage(null);
-        
-        // imagePath = null;
-        
+        productImageView.setImage(null);
         nameField.requestFocus();
     }
+
+
+    @FXML
+    private void handleImageSelection() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Profile Image");
+        FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Image Files", "*.png");
+        fileChooser.getExtensionFilters().add(imageFilter);
+
+        File file = fileChooser.showOpenDialog(AddImageButton.getScene().getWindow());
+
+        if (file != null) {
+            this.selectedImageFile = file;
+            Image newImage = new Image(selectedImageFile.toURI().toString());
+            productImageView.setImage(newImage);
+        }
+    }
+
+
+
+    
 }
